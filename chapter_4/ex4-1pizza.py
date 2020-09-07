4d86a020c9a0427e7b6c5bae2d5e5fa8a75fc643
alberthao <haohs@163.com>
2020-09-07 12:47:17 +0800

    update exercises to 4-2

---
+++ chapter_4/ex4-1pizza.py
@@ -0,0 +1,6 @@
+
+pizzas=["pepperoni pizza","vegetable pizza","Hawaiian pizza"]
+for pizza in pizzas:
+    print(f"I like {pizza}.")
+
+print("\nI really love pizza!")
---
+++ chapter_4/ex4-2animal.py
@@ -0,0 +1,6 @@
+animals=["dog","cat","pig"]
+for animal in animals:
+    print(animal)
+    print(f"A {animal} would make a great pet.")
+
+print("\nAny of these animals would make a great pet!")
