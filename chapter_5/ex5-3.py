5f84c81d5d6fb42222135feb5b624500f224ee77
Albert Hao <haohs@163.com>
2020-09-12 19:15:18 +0800

    ex506

---
+++ chapter_5/ex5-3.py
@@ -0,0 +1,13 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 5 points！")
+elif alien_color != 'green':
+    print("You failed!")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("You earned 5 points！")
+elif alien_color != 'green':
+    print("You failed!")
+
+
---
+++ chapter_5/ex5-4.py
@@ -0,0 +1,12 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 5 points！")
+elif alien_color != 'green':
+    print("You earned 10 points!")
+
+
+alien_color = 'green'
+if alien_color != 'green':
+    print("You earned 10 points！")
+elif alien_color == 'green':
+    print("You earned 5 points!")
